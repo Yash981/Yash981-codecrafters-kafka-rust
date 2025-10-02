495bac36bf64a62910e72dafc598ff991399b33d
Yashwanth <dyashwanth14011999@gmail.com>
2025-10-02 23:15:17 +0530

    read input from client & send sent correlation id

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,10 @@ fn main() {
         match stream {
             Ok(strm) => {
                 let mut streamm = strm;
-                let mut header_buf = [0u8; 12];
+                let mut header_buf = [0u8; 512];
                 streamm.read_exact(&mut header_buf).unwrap();
                 let correlation_id: i32 = i32::from_be_bytes(header_buf[8..12].try_into().unwrap());
                 let mut response = vec![];
-                response.extend_from_slice(&0i32.to_be_bytes()); // message_size
 
                 response.extend_from_slice(&0i32.to_be_bytes());
                 response.extend_from_slice(&correlation_id.to_be_bytes()); // correlation_id
