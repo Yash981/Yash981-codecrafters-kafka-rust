d32d7218e47a1c4055fe547e64edcd621d675d2b
Yashwanth <dyashwanth14011999@gmail.com>
2025-10-02 23:13:57 +0530

    read input from client & send sent correlation id

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![allow(unused_imports)]
-use std::{io::{Read, Write}, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,15 +16,16 @@ fn main() {
         match stream {
             Ok(strm) => {
                 let mut streamm = strm;
-                let mut header_buf = [0u8;12];
+                let mut header_buf = [0u8; 12];
                 streamm.read_exact(&mut header_buf).unwrap();
                 let correlation_id: i32 = i32::from_be_bytes(header_buf[8..12].try_into().unwrap());
                 let mut response = vec![];
+                response.extend_from_slice(&0i32.to_be_bytes()); // message_size
+
                 response.extend_from_slice(&0i32.to_be_bytes());
-                                response.extend_from_slice(&correlation_id.to_be_bytes()); // correlation_id
+                response.extend_from_slice(&correlation_id.to_be_bytes()); // correlation_id
 
                 streamm.write_all(&response).unwrap();
-
             }
             Err(e) => {
                 println!("error: {}", e);
